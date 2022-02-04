05b240a467b9949993bed4ee0b38475f152d9a4d
Dylan Cahoon <dylancahoon651@gmail.com>
2022-02-04 18:37:22 -0500

    commit with header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
+import { Route, Switch } from "react-router-dom"
 import Header from "./Header"
 import Home from "./Home"
 import Contact from "./Contact"
+import About from "./About"
 
 function App() {
   return (
@@ -12,12 +14,13 @@ function App() {
         <Route path={"/contact"}>
           <Contact />
         </Route>
+        <Route path={"/about"}>
+          <About />
+        </Route>
         <Route path={"/"}>
           <Home />
         </Route>
       </Switch>
-
-
 
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -3,10 +3,48 @@ import { Link } from "react-router-dom"
 export default function Header() {
     return (
         <div className="nav">
-            <h1 className="">{`{ ... } dylancahoonweb`}</h1>
-            <Link to={"/"} className="navlink">Home</Link>
-            <Link to={"/about"} className="navlink">About</Link>
-            <Link to={"/contact"} className="navlink">Contact</Link>
+            <h1 
+                className="
+                    logo
+                    col-sm-12
+                    col-lg-6
+                "
+            >
+                {`{ ... } dylancahoonweb`}
+            </h1>
+            <Link 
+                to={"/"} 
+                className="
+                    navlink
+                    col-sm-4
+                    col-lg-2
+                    align-self-center
+                "
+            >
+                HOME
+            </Link>
+            <Link 
+                to={"/about"} 
+                className="
+                    navlink
+                    col-sm-4
+                    col-lg-2
+                    align-self-center
+                "
+            >
+                ABOUT
+            </Link>
+            <Link 
+                to={"/contact"} 
+                className="
+                    navlink
+                    col-sm-4
+                    col-lg-2
+                    align-self-center
+                "
+            >
+                CONTACT
+            </Link>
         </div>
     )
 }
