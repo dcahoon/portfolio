885fbdac7f90cf16d674bb3c4afe387a6f10539e
Dylan Cahoon <dylancahoon651@gmail.com>
2022-02-10 18:36:22 -0500

    added links to contact page

--- src/About.js
+++ src/About.js
@@ -1,13 +1,60 @@
+import welovemovies_img from "./images/welovemovies.png"
 
 export default function About() {
     return (
-        <div>
-            This live preview for <a href="https://en.wikipedia.org/">Wikipedia</a>
-            <div class="box">
-                <iframe src="https://en.wikipedia.org/" width = "500px" height = "500px">
-                </iframe>
-            </div> 
-        </div>
+        <div className="row card-deck m-0 d-flex justify-content-center">
+            
+            <div className="col-sm-12 col-lg-10">
+                <div className="card">
+                    <div className="row card-body">
+                        <img className="col-sm-4 card-img" src={welovemovies_img} alt="sans"/>
+                        <div className="col-sm-8">
+                            <h3 className="card-title">WeLoveMovies App</h3>
+                            <p classname="card-text">This app demonstrates a server that handles requests for movies, reviews, and theaters. Please note that I only programmed the server for this app.</p>
+                            <p className="card-text">Node.js, Express.js, Knex, dotenv, CORS</p>
+                            <div className="row justify-content-around">
+                                <a className="btn btn-light col-5" href="https://obscure-dawn-57663.herokuapp.com/" target="_blank">Visit Site</a>
+                                <a className="btn btn-light col-5" href="https://github.com/dcahoon/welovemovies" target="_blank">GitHub Repository</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
+            <div className="col-sm-12 col-lg-10">
+                <div className="card">
+                    <div className="row card-body">
+                        <img className="col-sm-4 card-img" src={welovemovies_img} alt="sans"/>
+                        <div className="col-sm-8">
+                            <h3 className="card-title">WeLoveMovies App</h3>
+                            <p classname="card-text">This app demonstrates a server that handles requests for movies, reviews, and theaters. Please note that I only programmed the server for this app.</p>
+                            <p className="card-text">Node.js, Express.js, Knex, dotenv, CORS</p>
+                            <div className="row justify-content-around">
+                                <a className="btn btn-light col-5" href="https://obscure-dawn-57663.herokuapp.com/" target="_blank">Visit Site</a>
+                                <a className="btn btn-light col-5" href="https://github.com/dcahoon/welovemovies" target="_blank">GitHub Repository</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="col-sm-12 col-lg-10">
+                <div className="card">
+                    <div className="row card-body">
+                        <img className="col-sm-4 card-img" src={welovemovies_img} alt="sans"/>
+                        <div className="col-sm-8">
+                            <h3 className="card-title">WeLoveMovies App</h3>
+                            <p classname="card-text">This app demonstrates a server that handles requests for movies, reviews, and theaters. Please note that I only programmed the server for this app.</p>
+                            <p className="card-text">Node.js, Express.js, Knex, dotenv, CORS</p>
+                            <div className="row justify-content-around">
+                                <a className="btn btn-light col-5" href="https://obscure-dawn-57663.herokuapp.com/" target="_blank">Visit Site</a>
+                                <a className="btn btn-light col-5" href="https://github.com/dcahoon/welovemovies" target="_blank">GitHub Repository</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        
+        </div>
     )
 }
--- src/Contact.js
+++ src/Contact.js
@@ -7,15 +7,16 @@ export default function Contact() {
       }
     
     return (
-        <div className="p-4">
-            <a className="d-inline p-3" href="mailto:dylancahoon651@gmail.com">dylancahoon651@gmail.com</a>
-            <p className="h2 d-inline btn btn-secondary" onClick={copyToClipboard}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard-check" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
-                    <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
-                    <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
-                </svg>
-            </p>
+        <div className="link-container">
+            <div className="p-4 mt-5">
+                <a className="p-3 btn btn-light w-100" href="mailto:dylancahoon651@gmail.com">dylancahoon651@gmail.com</a>
+            </div>
+            <div className="p-4">
+                <a className="btn btn-light p-3 w-100" href="https://www.linkedin.com/in/dylancahoon/" target="_blank">LinkedIn</a>
+            </div>
+            <div className="p-4">
+                <a className="btn btn-light p-3 w-100" href="https://github.com/dcahoon" target="_blank">GitHub</a>
+            </div>
         </div>
     )
 }
