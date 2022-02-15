c2c27acdbad0256dc1f6e355ef204f350c2f5920
Dylan Cahoon <dylancahoon651@gmail.com>
2022-02-15 16:02:36 -0500

    removed template websites on about page

--- src/About.js
+++ src/About.js
@@ -61,40 +61,6 @@ export default function About() {
                         </div>
                     </div>
 
-                    <div className="col-sm-12 col-lg-10">
-                        <div className="card">
-                            <div className="row card-body">
-                                <img className="col-sm-4 card-img" src={welovemovies_img} alt="sans"/>
-                                <div className="col-sm-8 col-lg-10">
-                                    <h3 className="card-title">WeLoveMovies App</h3>
-                                    <p classname="card-text">This app demonstrates a server that handles requests for movies, reviews, and theaters. Please note that I only programmed the server for this app.</p>
-                                    <p className="card-text">Node.js, Express.js, Knex, dotenv, CORS</p>
-                                    <div className="row justify-content-around">
-                                        <a className="navlink col-5 border border-white" href="https://obscure-dawn-57663.herokuapp.com/" target="_blank">VISIT SITE</a>
-                                        <a className="navlink col-5 border border-white" href="https://github.com/dcahoon/welovemovies" target="_blank">GITHUB RESPOSITORY</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="col-sm-12 col-lg-10">
-                        <div className="card">
-                            <div className="row card-body">
-                                <img className="col-sm-4 card-img" src={welovemovies_img} alt="sans"/>
-                                <div className="col-sm-8 col-lg-10">
-                                    <h3 className="card-title">WeLoveMovies App</h3>
-                                    <p classname="card-text">This app demonstrates a server that handles requests for movies, reviews, and theaters. Please note that I only programmed the server for this app.</p>
-                                    <p className="card-text">Node.js, Express.js, Knex, dotenv, CORS</p>
-                                    <div className="row justify-content-around">
-                                        <a className="navlink col-5 border border-white" href="https://obscure-dawn-57663.herokuapp.com/" target="_blank">VISIT SITE</a>
-                                        <a className="navlink col-5 border border-white" href="https://github.com/dcahoon/welovemovies" target="_blank">GITHUB RESPOSITORY</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
                 </div>
             
             </div>
